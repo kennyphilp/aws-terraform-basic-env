a6d5fdd5b619dd36b360010ee942ee1f4a4b699f
Kenny Philp <kennyphilp@gmail.com>
2023-08-21 18:14:15 +0100

    Test

--- env_internet_access.tf
+++ env_internet_access.tf
@@ -1,22 +1,11 @@
-variable "awsprops" {
-  type = map(any)
-  default = {
-    region       = "us-east-1"
-    subnet       = "subnet-02653648ae120f8aa"
-    vpc          = "vpc-dev"
-    secgroupname = "IAC-Sec-Group"
 
-    env  = "dev"
-
+variable "environment" {
+  type = string
+  validation {
+    condition     = var.environment == "dev" || var.environment == "uat" || var.environment == "prod"
+    error_message = "Must specify a valid environment from (dev/uat/prod)"
   }
 }
-
-locals {
-  env = "dev"
-}
-
-
-
 
 provider "aws" {
   profile = "default"
@@ -24,45 +13,78 @@ provider "aws" {
 }
 
 
+variable "awsprops" {
+  type = map(any)
+  default = {
+    region        = "us-east-1"
+    secgroupname  = "IAC-Sec-Group"
+    key_pair_name = "KeyPairNorthVirginia"
+    ami           = "ami-08a52ddb321b32a8c"
+
+  }
+}
+
+module "assets" {
+  source        = "./modules"
+  environment   = var.environment
+  key_pair_name = var.awsprops.key_pair_name
+}
+
 resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support = true
-    enable_dns_hostnames = true
-    enable_classiclink = false
-    instance_tenancy = "default"
-    tags = {
-      Name     = "VPC - $(local.env}",
-      Teardown = "True"
-    }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  instance_tenancy     = "default"
+  tags = {
+    Name     = "vpc_${var.environment}",
+    Teardown = "True"
+  }
 }
 
 
 resource "aws_subnet" "subnet-public-1" {
-    vpc_id = "${aws_vpc.vpc.id}"
-    cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-east-1a"
-    tags = {
-      Name     = "VPC Instance 1",
-      Teardown = "True"
-    }
-}
-
-
-
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
   tags = {
-    Name = "main"
+    Name     = "Public_Subnet_1_${var.environment}",
+    Teardown = "True"
   }
 }
 
-resource "aws_security_group" "project-iac-sg" {
+resource "aws_route_table" "public-crt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    //associated subnet can reach everywhere
+    cidr_block = "0.0.0.0/0"
+
+    //CRT uses this IGW to reach internet
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+}
+
+resource "aws_route_table_association" "crta-subnet-public-1" {
+  subnet_id      = aws_subnet.subnet-public-1.id
+  route_table_id = aws_route_table.public-crt.id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "Internet_Gateway_${var.environment}"
+  }
+}
+
+
+
+resource "aws_security_group" "sg-ssh-allowed" {
   name        = lookup(var.awsprops, "secgroupname")
   description = lookup(var.awsprops, "secgroupname")
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id      = aws_vpc.vpc.id
 
   // To Allow SSH Transport
   ingress {
@@ -83,7 +105,7 @@ resource "aws_security_group" "project-iac-sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -92,26 +114,28 @@ resource "aws_security_group" "project-iac-sg" {
   }
 }
 
-
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
+
+  launch_template {
+    name = "t2_micro_template"
+  }
+
+  //ami = "ami-08a52ddb321b32a8c"
+  //instance_type = "t2.micro"
+
+
   associate_public_ip_address = true
-  subnet_id     = "${aws_subnet.subnet-public-1.id}"
-  user_data     = <<EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y httpd.x86_64
-    systemctl start httpd.service
-    systemctl enable httpd.service
-    echo “Hello World from $(hostname -f)” > /var/www/html/index.html
-  EOF
+  subnet_id                   = aws_subnet.subnet-public-1.id
+
+  vpc_security_group_ids = ["${aws_security_group.sg-ssh-allowed.id}"]
+
+  user_data = filebase64("${path.module}/scripts/user_data_httpd_install.sh")
 
   tags = {
-    Name     = "EC2 Instance 1",
+    Name     = "EC2_Instance_1_${var.environment}",
     Teardown = "True"
   }
 
-  depends_on = [aws_security_group.project-iac-sg, aws_internet_gateway.gw]
+  depends_on = [aws_security_group.sg-ssh-allowed, aws_internet_gateway.igw]
 
 }
